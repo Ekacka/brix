721ac47c75e0f4fd3dc486cba001e4378011ad4f
Ekacka <cfyz565419@gmail.com>
2024-11-06 22:40:12 +0500

    added a bin so deleted post are in archive

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ import service.PostService;
 import service.RuntimePostService;
 import service.ServiceManager;
 import service.EditPostCommand;
+import service.ArchivePostServiceDecorator;
+import java.util.List;
+
 
 import java.util.Date;
 import java.util.Scanner;
@@ -19,9 +22,12 @@ public class Main {
     public static void main(String[] args) {
 
         postRepo = new RuntimePostRepository();
-        postService = new RuntimePostService(postRepo);
-        serviceManager = new ServiceManager();
+        PostService runtimePostService = new RuntimePostService(postRepo);
 
+
+        postService = new ArchivePostServiceDecorator(runtimePostService);
+
+        serviceManager = new ServiceManager();
         serviceManager.registerService(postService);
 
         Scanner sc = new Scanner(System.in);
@@ -31,9 +37,10 @@ public class Main {
             switch (pressedKey) {
                 case 1 -> createPostMenu();
                 case 2 -> displayAllPosts();
-                case 3 -> deletePostMenu();
+                case 3 -> archivePostMenu();
                 case 4 -> editPostMenu();
                 case 5 -> undoLastAction();
+                case 6 -> displayArchivedPosts();
             }
         }
         sc.close();
@@ -41,24 +48,19 @@ public class Main {
 
     private static void createPostMenu() {
         PostBuilder builder = new PostBuilder();
-
-        String title;
-        String content;
-        String postedBy;
-
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Input the title.");
-        title = sc.nextLine();
+        String title = sc.nextLine();
         System.out.println("Input the content.");
-        content = sc.nextLine();
+        String content = sc.nextLine();
         System.out.println("Input the name of poster.");
-        postedBy = sc.nextLine();
+        String postedBy = sc.nextLine();
 
         builder.setCreatedAt(new Date());
-
         Post post = builder.setTitle(title).setContent(content).setPostedBy(postedBy).build();
-        postService.createPost(post);
 
+        postService.createPost(post);
         System.out.println("Post created successfully.");
         mainMenu();
     }
@@ -79,21 +81,37 @@ public class Main {
         mainMenu();
     }
 
-    private static void deletePostMenu() {
+    private static void archivePostMenu() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input id of post that you want to delete.");
+        System.out.println("Input id of post to archive:");
         long id = sc.nextLong();
 
         Post post = postService.getPostById(id);
         if (post != null) {
             postService.deletePost(id);
-            System.out.println("Post deleted successfully.");
+            System.out.println("Post archived successfully.");
         } else {
             System.out.println("Post with id " + id + " does not exist.");
         }
         mainMenu();
     }
 
+    private static void displayArchivedPosts() {
+        List<Post> archivedPosts = postService.getArchivedPosts();
+        if (archivedPosts.isEmpty()) {
+            System.out.println("No archived posts.");
+        } else {
+            for (Post post : archivedPosts) {
+                System.out.println("---------");
+                System.out.printf("Title: %s Id: %d%n", post.getTitle(), post.getId());
+                System.out.printf("Content: %s%n", post.getContent());
+                System.out.printf("Posted by: %s%n", post.getPostedBy());
+                System.out.printf("Created at: %s%n", post.getCreatedAt().toString());
+                System.out.println("---------");
+            }
+        }
+        mainMenu();
+    }
     private static void editPostMenu() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the ID of the post you want to edit:");
@@ -114,12 +132,12 @@ public class Main {
         System.out.println("Last action undone.");
         mainMenu();
     }
-
     private static void mainMenu() {
         System.out.println("Press 1 to create post.");
         System.out.println("Press 2 to see all posts.");
         System.out.println("Press 3 to delete post.");
         System.out.println("Press 4 to edit post.");
         System.out.println("Press 5 to undo last action.");
+        System.out.println("Press 6 to see bin.");
     }
 }
